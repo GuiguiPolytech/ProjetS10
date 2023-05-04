b5fb38ab6fdec92c0c7b7f5fefb7fffa6ad66164
GuiguiPolytech <guillaumemacie@gmail.com>
2023-05-04 11:04:40 +0200

    ok

---
+++ src/main/java/com/example/login/ProjetS10Application.java
@@ -0,0 +1,13 @@
+package com.example.login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetS10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetS10Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/model/Personne.java
@@ -0,0 +1,10 @@
+package com.example.model;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Personne {
+    private String email;
+    private String nom;
+    private String prenom;
+}
